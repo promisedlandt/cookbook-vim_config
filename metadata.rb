575bde988a79dc738870116184b797554025b67e
Nils Landt <nils@promisedlandt.de>
2012-06-20 17:08:09 +0200

    fixed long description in metadata

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ maintainer       "Nils Landt"
 maintainer_email "cookbooks@promisedlandt.de"
 license          "WTFPL"
 description      "Configures vim"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.2"
 depends           "vim"
