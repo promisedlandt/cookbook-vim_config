af4efb857ea1f94aab4ef4ea6271af65057aecff
Nils Landt <nils@promisedlandt.de>
2013-12-16 23:20:25 +0100

    Deprecate downloading scripts from vim.org
    Please use vim-scripts.org instead.

--- TODO.md
+++ TODO.md
@@ -12,7 +12,6 @@ Nice to have:
   * Test / adjust for RPM based distributions
   * Automatically add plugin manager boilerplate to config file (toggleable)
   * Get some user feedback
-  * Find a better way for vim site downloads - maybe use ark
   * Add delete action to LWRPs
 
 Probably stupid:
--- providers/vim.rb
+++ providers/vim.rb
@@ -1,8 +1,10 @@
-require 'fileutils'
-require 'open-uri'
+require "fileutils"
+require "open-uri"
 include VimSiteFiles
 
 action :create do
+  Chef::Log.warn("Installing scripts from vim.org is deprecated. Please use vim-scripts.org.")
+
   install_file_to_directory new_resource.name, new_resource.version, node[:vim_config][:bundle_dir], node[:vim_config][:owner], node[:vim_config][:owner_group], node[:vim_config][:force_bundle_update]
 
   new_resource.updated_by_last_action(true)
