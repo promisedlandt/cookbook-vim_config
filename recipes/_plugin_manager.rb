9822c184d44e728fcf79aa911498cb59df9d70c4
Alukardd <alukardd@alukardd.org>
2014-07-20 17:26:50 +0400

    Configure pathogen for autoload system-wide bundles

--- recipes/_plugin_manager.rb
+++ recipes/_plugin_manager.rb
@@ -1,3 +1,8 @@
 add_vim_git_plugin({ "vundle"   => "https://github.com/gmarik/vundle.git",
                      "unbundle" => "https://github.com/sunaku/vim-unbundle.git",
                      "pathogen" => "https://github.com/tpope/vim-pathogen.git" }[node[:vim_config][:plugin_manager].to_s])
+
+case node[:vim_config][:plugin_manager]
+  when "pathogen"
+    node.override[:vim_config][:vimrc][:config][:system_wide] = node[:vim_config][:vimrc][:config][:system_wide].dup << "" << "source /etc/vim/bundle/vim-pathogen/autoload/pathogen.vim" << "execute pathogen#infect('/etc/vim/bundle/{}', 'bundle/{}')"
+end
