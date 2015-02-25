2c0642bbc6b7105d5f815f1d484ce66eac03ee11
Nathan Hernandez <email@nathanph.com>
2015-02-24 20:49:04 -0500

    Update _plugin_manager.rb
    Updated Vundle git repository to its most recent version.

--- recipes/_plugin_manager.rb
+++ recipes/_plugin_manager.rb
@@ -1,3 +1,3 @@
-add_vim_git_plugin({ "vundle"   => "https://github.com/gmarik/vundle.git",
+add_vim_git_plugin({ "vundle"   => "https://github.com/gmarik/Vundle.vim.git",
                      "unbundle" => "git://github.com/sunaku/vim-unbundle.git",
                      "pathogen" => "git://github.com/tpope/vim-pathogen.git" }[node[:vim_config][:plugin_manager].to_s])
