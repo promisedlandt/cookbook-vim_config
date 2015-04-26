283b11c42ea5be7354e1cb3376afc49afe96ab3d
Nils Landt <nils@promisedlandt.de>
2015-04-26 15:57:04 +0200

    Add name to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
+name             "vim_config"
 maintainer       "Nils Landt"
 maintainer_email "cookbooks@promisedlandt.de"
 license          "MIT"
 description      "Configures vim and installs vim plugins"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version          "2.0.0"
 
 %w(git mercurial).each { |dep| depends dep }
