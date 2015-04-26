7571b1d01bec2b84a1bf062eff6339dcf044da49
Nils Landt <nils@promisedlandt.de>
2015-04-26 21:40:25 +0200

    Style fix

--- recipes/_plugin_manager.rb
+++ recipes/_plugin_manager.rb
@@ -4,5 +4,5 @@ add_vim_git_plugin({ "vundle"   => "https://github.com/gmarik/vundle.git",
 
 case node[:vim_config][:plugin_manager]
   when "pathogen"
-    node.override[:vim_config][:vimrc][:config][:system_wide] = node[:vim_config][:vimrc][:config][:system_wide].dup << "" << "source #{node[:vim_config][:bundle_dir]}/vim-pathogen/autoload/pathogen.vim" << "execute pathogen#infect('#{node[:vim_config][:bundle_dir]}/{}', 'bundle/{}')"
+    node.override[:vim_config][:vimrc][:config][:system_wide] = node[:vim_config][:vimrc][:config][:system_wide].dup << "" << "source #{ node[:vim_config][:bundle_dir] }/vim-pathogen/autoload/pathogen.vim" << "execute pathogen#infect('#{ node[:vim_config][:bundle_dir] }/{}', 'bundle/{}')"
 end
